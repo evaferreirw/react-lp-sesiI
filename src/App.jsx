7b12928e2e777cf1aaea30a321d1be31c3b381c5
evaferreirw <evangeline.ferreira.1@gmail.com>
2026-05-22 11:07:40 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,24 @@ function App() {
 
   return (
    <>
+     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link" href="#">Pricing</a>
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </div>
+        </div>
+    </div>
+</nav>
 
-    <nav>
+    {/* <nav>
         <div className={s.topnav}>
             <img src={nomeDaFoto} alt="" width="85px"/>
                 
@@ -42,7 +58,7 @@ function App() {
                 <a href="#s10">Presentes</a>
             </span>
         </div>
-    </nav>
+    </nav> */}
     <main>
         <section id="s1" className={s.s1}>
            
@@ -51,13 +67,17 @@ function App() {
             <h1 className={s.titulo}>Conheça nossos ícones</h1>
             <h2 className={s.subtitulo}>3d Hydra Lipgloss</h2>
             <h3 className={s.paragrafo}>Uma nova fronteira de luminosidade e conforto para os seus lábios. Um produto simplesmente icónico e belo que hidrata os lábios até 8h, envolvendo-os num brilho puro</h3>
-            <h1 className= {s.subtitulo2}>Unlimited Double Touch</h1>
+            <h2 className= {s.subtitulo2}>Unlimited Double Touch</h2>
             <h3 className={s.paragrafo2}> Batom líquido de longa duração (16 horas*) em 2 passos, acabamento luminoso</h3>
+            <h2 className={s.subtitulo3}>New Maxi Mod Mascara </h2>
+            <h3 className={s.paragrafo3}> Obtenha um efeito de volume 200% impulsionado* com esta máscara arrojada e de longa duração. Com uma fixação até 10 horas** e um aplicador pequeno e preciso, obterá umas pestanas incrivelmente longas de longa duração.</h3>
+
         </section>
         <section id="s3" className={s.s3}>
             <iframe  className={s.video} src="https://www.youtube.com/embed/YOALdVJ-am4?si=R07qCnwI0u_z4AsP&autoplay=1&mute=1"  title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </section>
         <section id="s4" className={s.s4}>
+            <h1 className={s.titulo2}> Coleções e Lançamentos</h1>
 
         </section>
         <section id="s5" className={s.s5}>
